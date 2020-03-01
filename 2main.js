ec74b1f5c23334fc1ad55a3da385cc53d94387a0
Hiko5348 <56217005+Hiko5348@users.noreply.github.com>
2020-03-01 11:46:28 +0300

    01.03.2020

---
+++ 2main.js
@@ -0,0 +1,25 @@
+function Getremainder () {
+    console.log("Water the plants");
+}
+function GreetInSpanish() {
+    console.log("Buenas Tardes");
+}
+Getremainder();
+GreetInSpanish();
+function sayThanks(name) {
+    console.log(name + ", Thank you for your purcase!WE appretiate your bussines");
+}
+sayThanks("Ivan");
+
+sayThanks();
+sayThanks();
+sayThanks(); 
+function monitorcount(columns,rows) {
+    const result=rows*columns;
+    return result;
+}
+
+    let nums=[1,2,3,4,5,];
+    console.log(nums);
+   let elm=["element example",10 , true]
+    console.log(elm);
